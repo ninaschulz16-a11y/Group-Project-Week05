fa236d75f4419ca5163cefe12edfd648b8de663c
Dale Simkin <dale.simkin32@gmail.com>
2025-09-30 09:43:29 +0100

    const delarations for DOM manipulation

---
+++ client/src/main.js
@@ -0,0 +1,5 @@
+// declare DOM constants
+
+const mainDisplaySection = document.getElementById("app");
+const search = document.getElementById("search");
+const form = document.getElementById("form");
